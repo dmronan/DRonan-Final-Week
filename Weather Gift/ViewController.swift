7c08813249b077ca5ed39d8ae68f4db94a34036d
Dom Ronan <dominickronan@doms-mbp.bc.edu>
2017-03-19 17:56:43 -0400

    Initial Commit

---
+++ Weather Gift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Gift
+//
+//  Created by Dom Ronan on 3/19/17.
+//  Copyright © 2017 Dom Ronan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
