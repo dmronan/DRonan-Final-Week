3edcb796bf839a2095e035ab63dccf3b3dce1ea1
Dom Ronan <dominickronan@doms-mbp.bc.edu>
2017-03-19 20:48:50 -0400

    UI Setup

---
+++ Weather Gift/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  Weather Gift
+//
+//  Created by Dom Ronan on 3/19/17.
+//  Copyright © 2017 Dom Ronan. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
---
+++ Weather Gift/ListVC.swift
@@ -0,0 +1,20 @@
+//
+//  ListVC.swift
+//  Weather Gift
+//
+//  Created by Dom Ronan on 3/19/17.
+//  Copyright © 2017 Dom Ronan. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
